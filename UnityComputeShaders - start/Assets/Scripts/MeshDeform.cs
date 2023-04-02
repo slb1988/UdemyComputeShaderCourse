f38bd37137ef7273ada1bfe20ea493fd5c927b2d
Luckey_Sun <sunlaibing88@gmail.com>
2023-04-03 00:21:51 +0800

    update 2_5
    注意 Dispatch(kernelHandle, vertexArray.Length

--- UnityComputeShaders - start/Assets/Scripts/MeshDeform.cs
+++ UnityComputeShaders - start/Assets/Scripts/MeshDeform.cs
@@ -7,9 +7,32 @@ public class MeshDeform : MonoBehaviour
     public ComputeShader shader;
     [Range(0.5f, 2.0f)]
 	public float radius;
+    
+    public struct Vertex
+    {
+        public Vector3 position;
+        public Vector3 normal;
+
+        public Vertex(Vector3 p, Vector3 n)
+        {
+            position.x = p.x;
+            position.y = p.y;
+            position.z = p.z;
+            normal.x = n.x;
+            normal.y = n.y;
+            normal.z = n.z;
+        }
+    }
 	
     int kernelHandle;
     Mesh mesh;
+
+    private Vertex[] vertexArray;
+
+    private Vertex[] initialArray;
+
+    private ComputeBuffer vertexBuffer;
+    private ComputeBuffer initialBuffer;
     
     // Use this for initialization
     void Start()
@@ -49,17 +72,45 @@ public class MeshDeform : MonoBehaviour
     
     private void InitVertexArrays(Mesh mesh)
     {
-        
+        vertexArray = new Vertex[mesh.vertexCount];
+        initialArray = new Vertex[mesh.vertexCount];
+
+        var vertices = mesh.vertices;
+        var normals = mesh.normals;
+        for (int i = 0; i < mesh.vertexCount; i++)
+        {
+            var v1 = new Vertex(vertices[i], normals[i]);
+            vertexArray[i] = v1;
+            initialArray[i] = v1;
+        }
     }
 
     private void InitGPUBuffers()
     {
+        vertexBuffer = new ComputeBuffer(vertexArray.Length, sizeof(float) * 6);
+        vertexBuffer.SetData(vertexArray);
+
+        initialBuffer = new ComputeBuffer(initialArray.Length, sizeof(float) * 6);
+        initialBuffer.SetData(vertexArray);
         
+        shader.SetBuffer(kernelHandle, "vertexBuffer", vertexBuffer);
+        shader.SetBuffer(kernelHandle, "initialBuffer", initialBuffer);
     }
     
     void GetVerticesFromGPU()
     {
-        
+        vertexBuffer.GetData(vertexArray);
+        Vector3[] vertices = new Vector3[vertexArray.Length];
+        Vector3[] normals = new Vector3[vertexArray.Length];
+
+        for (int i = 0; i < vertexArray.Length; i++)
+        {
+            vertices[i] = vertexArray[i].position;
+            normals[i] = vertexArray[i].normal;
+        }
+
+        mesh.vertices = vertices;
+        mesh.normals = normals;
     }
 
     void Update(){
@@ -68,7 +119,7 @@ public class MeshDeform : MonoBehaviour
         	shader.SetFloat("radius", radius);
             float delta = (Mathf.Sin(Time.time) + 1)/ 2;
             shader.SetFloat("delta", delta);
-            shader.Dispatch(kernelHandle, 1, 1, 1);
+            shader.Dispatch(kernelHandle, vertexArray.Length, 1, 1);
             
             GetVerticesFromGPU();
         }
