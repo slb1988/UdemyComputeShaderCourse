83e73f72ffcf35cc4d345e8f0686a4844524ed5c
Luckey_Sun <sunlaibing88@gmail.com>
2023-04-02 22:07:53 +0800

    update 2_2

--- UnityComputeShaders - start/Assets/Scripts/BufferJoy.cs
+++ UnityComputeShaders - start/Assets/Scripts/BufferJoy.cs
@@ -13,10 +13,19 @@ public class BufferJoy : MonoBehaviour
     int circlesHandle;
     int clearHandle;
 
+    struct Circle
+    {
+        public Vector2 origin;
+        public Vector2 velocity;
+        public float radius;
+    }
+    
     public Color clearColor = new Color();
     public Color circleColor = new Color();
 
     int count = 10;
+    private Circle[] circleData;
+    private ComputeBuffer buffer;
 
     // Use this for initialization
     void Start()
@@ -36,6 +45,29 @@ public class BufferJoy : MonoBehaviour
     private void InitData()
     {
         circlesHandle = shader.FindKernel("Circles");
+        
+        shader.GetKernelThreadGroupSizes(circlesHandle,
+            out uint threadGroupSizeX, out _, out _);
+
+        int total = (int)threadGroupSizeX * count;
+        circleData = new Circle[total];
+
+        float speed = 100;
+        float halfSpeed = speed * 0.5f;
+        float minRadius = 10.0f;
+        float maxRadius = 30.0f;
+        float radiusRange = maxRadius - minRadius;
+
+        for (int i = 0; i < total; i++)
+        {
+            Circle circle = circleData[i];
+            circle.origin.x = Random.value * texResolution;
+            circle.origin.y = Random.value * texResolution;
+            circle.velocity.x = (Random.value * speed) - halfSpeed;
+            circle.velocity.y = (Random.value * speed) - halfSpeed;
+            circle.radius = Random.value * radiusRange + minRadius;
+            circleData[i] = circle;
+        }
     }
 
     private void InitShader()
@@ -50,6 +82,12 @@ public class BufferJoy : MonoBehaviour
         shader.SetTexture( circlesHandle, "Result", outputTexture );
 
         rend.material.SetTexture("_MainTex", outputTexture);
+
+        // 跟strcut大小一致
+        int stride = (2 + 2 + 1) * sizeof(float);
+        buffer = new ComputeBuffer(circleData.Length, stride);
+        buffer.SetData(circleData);
+        shader.SetBuffer(circlesHandle, "circlesBuffer", buffer);
     }
  
     private void DispatchKernels(int count)
